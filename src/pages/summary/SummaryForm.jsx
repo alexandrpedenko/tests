1355b6c3eaa68fd7df14a8b94208c629cf34a041
alexandrpedenko <alexandrpedenko@gmail.com>
2021-04-29 17:56:39 +0300

    Add first test

---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function SummaryForm() {
+  const [toChecked, setToChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId='terms-and-conditions'>
+        <Form.Check
+          type='checkbox'
+          checked={toChecked}
+          onChange={(e) => setToChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit' disabled={!toChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+}
---
+++ src/pages/summary/test/SummaryFrom.test.jsx
@@ -0,0 +1,18 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
+
+test('Initial conditions', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
