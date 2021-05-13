1133558718950646dbb5983db49cfd278cca196c
alexandrpedenko <alexandrpedenko@gmail.com>
2021-05-13 13:58:44 +0300

    Setup tests

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => console.log(error));
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
