e2268a6dacb36b0cfaff481e9a141db03a6de269
alexandrpedenko <alexandrpedenko@gmail.com>
2021-05-14 19:00:54 +0300

    Add Context Wrapper Testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 import OrderEntry from './pages/entry/OrderEntry';
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <OrderEntry />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
   );
 }
 
